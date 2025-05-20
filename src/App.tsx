5a258a351f9afaf18d7698124f5b877c1f4d0c9a
Tareq <tareqaldhaifani@gmail.com>
2025-05-20 19:33:58 +0400

    shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className="flex h-screen items-center justify-center">
+      <Button>Hello</Button>
+    </div>
   );
 }
 
